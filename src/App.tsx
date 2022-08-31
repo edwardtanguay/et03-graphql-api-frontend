2263488afa788b42b9d800ab9b371578d79f946d
Edward Tanguay (windows) <edward@tanguay.info>
2022-08-31 11:44:08 +0200

    showing graphql message

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.scss';
 import axios from 'axios';
+import { useQuery, gql } from '@apollo/client';
 
 const backendEmployeesUrl = 'http://localhost:5424/employees';
 
@@ -10,8 +11,15 @@ interface IEmployee {
 	territoryIDs: number[];
 }
 
+const QUERY = gql`
+	{
+		message
+	}
+`;
+
 function App() {
 	const [employees, setEmployees] = useState([]);
+	const { loading, data } = useQuery(QUERY);
 
 	useEffect(() => {
 		(async () => {
@@ -21,7 +29,9 @@ function App() {
 
 	return (
 		<div className="App">
-			<h1>GraphQL Demo</h1>
+			<h1>GraphQL and REST Demo</h1>
+			<h2>Data from GraphQL API</h2>
+			{!loading && <p>{data.message}</p>}
 			<h2>Data from REST API</h2>
 			<h3>There are {employees.length} employees</h3>
 			<ul>
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+	uri: 'http://localhost:5424/graphql',
+	cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-    <App />
-)
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>
+);
